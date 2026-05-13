e443a002b59364e0cd3610c0d3ae03464fa50f03
eityroy <eityroy027@gmail.com>
2026-05-13 11:01:05 +0600

    react password genarator+copy project

--- react-password-genarator-project/src/App.jsx
+++ react-password-genarator-project/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     for(let i=1;i<=length;i++){
      let char=Math.floor( Math.random()*str.length)
 
-     pass+=str.charAt(char)
+     pass+=str.charAt(char);
     }
 
     setPassword(pass);
